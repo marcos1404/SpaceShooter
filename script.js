705cb35b0c9d341b6805abcd3078cb36ddbceff7
marcos1404 <mv.99123.mv@gmail.com>
2021-01-21 17:03:29 -0300

    spaceshooter

---
+++ script.js
@@ -0,0 +1,85 @@
+const yourShip = document.querySelector('.player-shooter')
+const playArea = document.querySelector('#main-play-area')
+const aliensImg = ['img/monster-1.png','img/monster-2.png','monster-3.png']
+
+function flyShip(event){
+    if(event.key === "ArrowUp") {
+        event.preventDefault()
+        moveUp()
+    }
+    else if(event.key === "ArrowDown") {
+        event.preventDefault()
+        moveDown()
+    }
+    else if(event.key === " ") {
+        event.preventDefault()
+        fireLaser()
+    }
+}
+
+function moveUp(){
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top')
+    if(topPosition === '0px'){
+        return
+    }
+    else {
+        let position = parseInt(topPosition)
+        position -= 50
+        yourShip.style.top = `${position}px`
+    }
+
+}
+function moveDown(){
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top')
+    if(topPosition === '500px'){
+        return
+    }
+    else { 
+        let position = parseInt(topPosition)
+        position += 50
+        yourShip.style.top = `${position}px`
+    }
+}
+function fireLaser(){
+    let laser = createLaserElement()
+    playArea.appendChild(laser)
+    moveLaser(laser)
+}
+function createLaserElement(){
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'))
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'))
+    let newLaser = document.createElement('img') 
+    newLaser.src = 'img/shoot.png'
+    newLaser.classList.add('laser')
+    newLaser.style.left = `${xPosition}px`
+    newLaser.style.top = `${yPosition - 10}px`
+    return newLaser
+
+}
+function moveLaser(laser){
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left)
+
+        if(xPosition === 340) {
+            laser.remove()
+        }
+        else {
+            laser.style.left = `${xPosition + 8}px`
+        }
+    },10)
+}
+
+function createAliens(){
+    let newAlien = document.createElement('img')
+    let alienSprite = aliensImg[Math.floor(Math.random() * aliensImg.length)]
+    newAlien.src = alienSprite
+    newAlien.classList.add('alien')
+    newAlien.classList.add('alien-transition')
+    newAlien.style.left = '370px'
+    newAlien.style.top = `${Math.floor(Math.random() * 330) + 30}px`
+    playArea.appendChild(newAlien)
+    novoAlien(newAlien)
+}
+
+window.addEventListener('keydown', flyShip)
+
